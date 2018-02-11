02bc42da9b2af03b984355b52b517395720d1e3f
Japoy Padilla <jasper.padilla@entropysolution.com>
2018-02-11 15:53:52 +0800

    first commit
    first commit

---
+++ worklog/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from worklog.models import Worklog
+
+# Register your models here.
+admin.site.register(Worklog)
---
+++ worklogger/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+def login_redirect(request):
+    return redirect(reverse('worklog:index'))
