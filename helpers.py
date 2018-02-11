a02c34e77be5634efa36a280822f1193ede1689a
Japoy Padilla <jasper.padilla@entropysolution.com>
2018-02-11 15:51:06 +0800

    Revert "first commit"
    This reverts commit 33bbb5e16e835a7a7432cbf88f55f05ef9e7fdc4.

--- helpers.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def convert_timestamp_to_date(input_val, date_format='%m/%d/%Y %H:%M'):
-	try:
-		input_val = time.strftime(date_format)
-	except:
-		return None
-	return input_val
--- worklog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from worklog.views import index, add_worklog, get_worklog, delete_worklog
-
-urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^add_worklog/$', add_worklog, name='add_worklog'),
-    url(r'^get_worklog/$', get_worklog, name='get_worklog'),
-    url(r'^delete_worklog/$', delete_worklog, name='delete_worklog'),
-]
